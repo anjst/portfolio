ed034c70be096c6598cce63414dc3617f5a2978d
antanas <antanas24@gmail.com>
2020-11-30 23:48:01 +0200

    finished portfolio

--- script/script.js
+++ script/script.js
@@ -3,9 +3,13 @@ function replaceword(that, word, oword) {
     if (that.textContent === word) {
         const ul = document.getElementById('list');
         const li = document.createElement('li');
-        const contenta = ':)';
-        li.appendChild(document.createTextNode(contenta));
-        ul.appendChild(li);
+        const x = document.createElement("IMG");
+        x.setAttribute("src", "./lol.gif");
+        x.setAttribute("width", "100");
+        x.setAttribute("height", "80");
+        x.setAttribute("alt", "The Pulpit Rock");
+        document.body.appendChild(x);
+        ul.appendChild(x);
         that.textContent = oword;
     } else {
         let ul = document.getElementById('list');
