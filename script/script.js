4539d7fcbb15d21f43c936bf18d78a7ca55ac4cd
antanas <antanas24@gmail.com>
2020-12-02 02:14:52 +0200

    lololo display-none version

--- script/script.js
+++ script/script.js
@@ -18,3 +18,12 @@ function replaceword(that, word, oword) {
     }
 }
 
+function replace() {
+    const x = document.getElementById("lolo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
